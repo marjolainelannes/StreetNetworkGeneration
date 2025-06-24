8bd18d0389751fd01606bb3866cdf7d350c6b624
Marjolaine Lannes <78964700+marjolainelannes@users.noreply.github.com>
2025-06-24 18:08:11 +0200

    Create adjacent_cells.py

---
+++ src/data/urban_form/adjacent_cells.py
@@ -0,0 +1,45 @@
+###################################################################################
+# Study: Street network generation
+# Purpose: Get adjacent cells
+###################################################################################
+import pickle
+
+# Ouput
+path = "../../../"
+f_out = path + "data/grid_data/adjacent_cells.dat"
+
+# Parameters
+Nx=110
+Ny=75
+
+# Get a list of adjacent cells for each cell
+N_cells = Nx * Ny
+adjacent_cells = {}
+for C in range(N_cells):
+    # We write C = y * Nx + x
+    y = int(C/Nx)
+    x = C % Nx
+    # do we have to add right/left cells ?
+    x_iterate = []
+    if x > 0 :
+        x_iterate.append(x-1)
+    x_iterate.append(x)
+    if x < Nx-1 :
+        x_iterate.append(x+1)
+    # add upper cells
+    adjacent_cell_C = []
+    if (y > 0)  :
+        for k in x_iterate:
+            adjacent_cell_C.append((y-1) * Nx + k)
+    # add cells on the same longitude
+    for k in x_iterate:
+        adjacent_cell_C.append(y * Nx + k)
+    # add bottom cells
+    if (y < Ny-1):
+        for k in x_iterate:
+            adjacent_cell_C.append((y + 1) * Nx + k)
+    # add cell C adjacency list to dict
+    adjacent_cells[C] = adjacent_cell_C
+
+# Save it
+pickle.dump(adjacent_cells, open(f_out, "wb"))
