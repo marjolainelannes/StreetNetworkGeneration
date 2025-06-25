240a280c502ffc1083f504c393b63b938df9c159
Marjolaine Lannes <78964700+marjolainelannes@users.noreply.github.com>
2025-06-25 12:38:49 +0200

    Delete include/create_circles.py

--- include/create_circles.py
+++
@@ -1,8 +0,0 @@
-def create_circles_around_road_nodes(xa, xb, ya, yb, radius) :
-    from shapely.geometry import Point, Polygon
-    node_A = Point(xa,ya)
-    node_B = Point(xb, yb)
-    circle_A = node_A.buffer(radius) # type is polygon
-    circle_B = node_B.buffer(radius)
-    return(circle_A, circle_B)
-
