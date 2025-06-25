75ae5a5ae6daa891a0b19f2df671757ee38a9819
Marjolaine Lannes <78964700+marjolainelannes@users.noreply.github.com>
2025-06-25 12:47:07 +0200

    Update create_poly_around_road.py

--- include/create_poly_around_road.py
+++ include/create_poly_around_road.py
@@ -1,6 +1,36 @@
+from shapely.geometry import Polygon
+
+# Function to get the coordinates of the 4 summits of the polygon surrounding the road with xa =/ xb and ya =/ yb
+def xycoord_dist_x0_y0(a,xa,ya,xb,yb,dist):
+    #x,y coordinates on line2 with distance dist from (x0,y0)
+    import numpy as np
+    if a > 0 :
+       x1 = xa - dist * np.sqrt(a**2/(a**2+1))
+       y1 = ya + dist / (np.sqrt(a**2+1))
+
+       x2 = xa + dist * np.sqrt(a**2/(a**2+1))
+       y2 = ya - dist / (np.sqrt(a**2+1))
+
+       x3 = xb + dist * np.sqrt(a**2/(a**2+1))
+       y3 = yb - dist / (np.sqrt(a**2+1))
+
+       x4 = xb - dist * np.sqrt(a**2/(a**2+1))
+       y4 = yb + dist / (np.sqrt(a**2+1))
+    if a < 0 :
+       x1 = xa - dist * np.sqrt(a**2/(a**2+1))
+       y1 = ya - dist / (np.sqrt(a**2+1))
+
+       x2 = xa + dist * np.sqrt(a**2/(a**2+1))
+       y2 = ya + dist / (np.sqrt(a**2+1))
+
+       x3 = xb + dist * np.sqrt(a**2/(a**2+1))
+       y3 = yb + dist / (np.sqrt(a**2+1))
+
+       x4 = xb - dist * np.sqrt(a**2/(a**2+1))
+       y4 = yb - dist / (np.sqrt(a**2+1))
+    return ((x1,y1),(x2,y2),(x3,y3),(x4,y4))
+
 def create_2_poly_around_road(xa,ya,xb,yb,width):
-    from shapely.geometry import Polygon
-    from include.get_poly_coordinates import xycoord_dist_x0_y0
     # Get the polygon coordinates
     if xa == xb :  # vertical axis
                 (x1,y1)=(xa-width,ya)
@@ -21,8 +51,6 @@ def create_2_poly_around_road(xa,ya,xb,yb,width):
     return([road_poly_1, road_poly_2])
 
 def create_1_poly_around_road(xa,ya,xb,yb,width):
-    from shapely.geometry import Polygon
-    from include.get_poly_coordinates import xycoord_dist_x0_y0
     # Get the polygon coordinates
     if xa == xb :  # vertical axis
                 (x1,y1)=(xa-width,ya)
