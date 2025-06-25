bda1ee63994d385168aa5e20c668ecf1ffd0e451
Marjolaine Lannes <78964700+marjolainelannes@users.noreply.github.com>
2025-06-25 12:44:34 +0200

    Update get_poly_coordinates.py

--- include/get_poly_coordinates.py
+++ include/get_poly_coordinates.py
@@ -28,18 +28,3 @@ def xycoord_dist_x0_y0(a,xa,ya,xb,yb,dist):
        x4 = xb - dist * np.sqrt(a**2/(a**2+1))
        y4 = yb - dist / (np.sqrt(a**2+1))
     return ((x1,y1),(x2,y2),(x3,y3),(x4,y4))
-
-# à supprimer : il y a déjà la fonction interpolate qui le fair
-def get_C_from_line_dist(line, dist) :
-    from shapely.geometry import LineString  # usefull for line.coords
-    (xc, yc) = line.interpolate(dist).coords[0]
-    # slope = (yb - ya) / (xb - xa)
-    # import numpy as np
-    # num_vert = int(round(line.length / dist))
-    # if num_vert == 0:
-    #     num_vert = 1
-    #     # geom.interpolate(dist)
-    # return LineString(
-    #     [line.interpolate(float(n) / num_vert, normalized=True)
-    #      for n in range(num_vert + 1)])
-    return([xc,yc])
