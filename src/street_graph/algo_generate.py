8d0dc9829f3260b3800b708c23087c5c13feb5ef
Marjolaine Lannes <78964700+marjolainelannes@users.noreply.github.com>
2025-06-25 12:03:41 +0200

    Update algo_generate.py

--- src/street_graph/algo_generate.py
+++ src/street_graph/algo_generate.py
@@ -12,14 +12,14 @@ output_1 = "Table_of_cells_1.dat"
 output_2 = "Table_of_cells_2.dat"
 n = N_cells/2
 list_1 = []
-list_l = []
+list_2 = []
 for i in range (0,n):
     list_1.append(i)
     list_2.append(n+i)
-df_h = pd.DataFrame({'NCELL': list_u})
-df_h.to_csv(output_1, index=False)
-df_l = pd.DataFrame({'NCELL': list_l})
-df_l.to_csv(output_2, index=False)
+df_1 = pd.DataFrame({'NCELL': list_1})
+df_1.to_csv(output_1, index=False)
+df_2 = pd.DataFrame({'NCELL': list_2})
+df_2.to_csv(output_2, index=False)
 
 ## Nodes
 computers = ['computer_1','computer_2']
